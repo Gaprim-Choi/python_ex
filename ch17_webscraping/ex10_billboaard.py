6bd7920b035b7592744b4217aaa8ac389b0479c8
Gaprim-Choi <79235694+Gaprim-Choi@users.noreply.github.com>
2021-11-25 18:01:40 +0900

    main

--- ch17_webscraping/ex10_billboaard.py
+++ ch17_webscraping/ex10_billboaard.py
@@ -8,36 +8,16 @@ html_music = requests.get(url).text
 soup_music = BeautifulSoup(html_music, "lxml")
 
 titles = soup_music.select('h3.a-no-trucate')
-print(titles[0:7])
+artists = soup_music.select('span.a-no-trucate')
 
 music_titles = [title.get_text().strip() for title in titles]
-print(music_titles[0:7])
-
-artists = soup_music.select('span.a-no-trucate')
-print(artists[0:7])
-
-""" artists = soup_music.select('span.a-no-trucate')
-print(artists[0:7]) """
 music_artists = [artists.get_text().strip() for artists in artists]
-print(music_artists[0:7])
-
-for k in range(7):
-    print("{0}: {1} / {2}".format(k+1, music_titles[k], music_artists[k]))
-
-music_titles_artists = {}
-order = 0
-
-for (music_title, music_artist) in zip(music_titles, music_artists):
-    order = order + 1
-    music_titles_artists[order] = [music_title, music_artist]
-
-print(music_titles_artists[1])
 
 file_name = './ch17_webscraping/billboard_top100.txt'
 
 f = open(file_name, 'w', encoding="utf-8")
 
-for k in range(len(music_titles_artists)):
+for k in range(len(music_titles)):
     f.write("{0:2d}: {1}/{2}\n".format(k+1,
                                        music_titles[k],  music_artists[k]))
 
