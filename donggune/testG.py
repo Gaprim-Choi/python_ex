365cd7ba09ade02e26057b359d3427f1476ec474
Gaprim-Choi <79235694+Gaprim-Choi@users.noreply.github.com>
2021-12-01 14:33:33 +0900

    main

---
+++ donggune/testG.py
@@ -0,0 +1,64 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+ 
+from math import pi
+from matplotlib.path import Path
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+ 
+## 데이터 준비
+df = pd.DataFrame({
+'Character': ['기준','입력'],
+'Strength': [10, 5],
+'Dexterity': [4, 10],
+'Vitality': [9, 9],
+'Energy': [4, 4],
+'Wisdom': [2, 6]
+})
+
+## 하나로 합치기 - 폴리곤
+labels = df.columns[1:]
+num_labels = len(labels)
+    
+angles = [x/float(num_labels)*(2*pi) for x in range(num_labels)] ## 각 등분점
+angles += angles[:1] ## 시작점으로 다시 돌아와야하므로 시작점 추가
+    
+my_palette = plt.cm.get_cmap("Set2", len(df.index))
+ 
+fig = plt.figure(figsize=(8,8))
+fig.set_facecolor('white')
+ax = fig.add_subplot(polar=True)
+for i, row in df.iterrows():
+    color = my_palette(i)
+    data = df.iloc[i].drop('Character').tolist()
+    data += data[:1]
+    
+    ax.set_theta_offset(pi / 2) ## 시작점
+    ax.set_theta_direction(-1) ## 그려지는 방향 시계방향
+    
+    plt.xticks(angles[:-1], labels, fontsize=13) ## x축 눈금 라벨
+    ax.tick_params(axis='x', which='major', pad=15) ## x축과 눈금 사이에 여백을 준다.
+    ax.set_rlabel_position(0) ## y축 각도 설정(degree 단위)
+    plt.yticks([0,2,4,6,8,10],['0','2','4','6','8','10'], fontsize=10) ## y축 눈금 설정
+    plt.ylim(0,10)
+    
+    ax.plot(angles, data, color=color, linewidth=2, linestyle='solid', label=row.Character) ## 레이더 차트 출력
+    ax.fill(angles, data, color=color, alpha=0.4) ## 도형 안쪽에 색을 채워준다.
+    
+for g in ax.yaxis.get_gridlines(): ## grid line 
+    g.get_path()._interpolation_steps = len(labels)
+ 
+spine = Spine(axes=ax,
+          spine_type='circle',
+          path=Path.unit_regular_polygon(len(labels)))
+ 
+## Axes의 중심과 반지름을 맞춰준다.
+spine.set_transform(Affine2D().scale(.5).translate(.5, .5)+ax.transAxes)
+           
+ax.spines = {'polar':spine} ## frame의 모양을 원에서 폴리곤으로 바꿔줘야한다.
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'  # '맑은 고딕'으로 설정
+matplotlib.rcParams['axes.unicode_minus'] = False 
+plt.legend(loc=(0.9,0.9))
+plt.show()
---
+++ donggune/점수 계산.py
@@ -0,0 +1,41 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_excel('./donggune/testUser.xlsx', sheet_name=0)
+jj = pd.read_excel('./donggune/testUser.xlsx', sheet_name=1)
+jm = pd.read_excel('./donggune/testUser.xlsx', sheet_name=2)
+
+# 직업 적성에 대한 각 항목에 대한 데이터값 연산 전 정리
+jj = jj.replace('A', 3)
+jj = jj.replace('B', 2)
+jj = jj.replace('C', 1)
+jj = jj.drop(['문항'],axis=1)
+print(jj)
+
+# 직업 만족도에 대한 각 항목에 대한 데이터값 연산 전 정리
+jm = jm.replace('A', 3)
+jm = jm.replace('B', 2)
+jm = jm.replace('C', 1)
+jm = jm.drop(['문항'],axis=1)
+
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'  # '맑은 고딕'으로 설정
+matplotlib.rcParams['axes.unicode_minus'] = False
+matplotlib.rcParams["figure.figsize"] = (15, 4)
+
+product_plot = jj.plot(grid=True, title='각 문항별 점수')
+product_plot = jm.plot(grid=True, title='각 문항별 점수')
+plt.ylim([0, 5])
+plt.show()
+
+
+jj = jj.transpose()
+jm = jm.transpose()
+
+# 만족도 조사에 대한 합산 값 연산
+jj_sum = pd.DataFrame(jj.sum(axis=1)/3, columns=['평균'])
+jm_sum = pd.DataFrame(jm.sum(axis=1)/3, columns=['평균'])
+
+# 각 설문에 대한 답변 점수의 총 합산값 출력
+print('당신의 직업 적성 평가는\n' ,jj_sum.loc['답변'])
+print('당신의 직업 만족도 평가는\n',jm_sum.loc['답변'])
