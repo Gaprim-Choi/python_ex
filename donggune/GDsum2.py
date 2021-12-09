4cd5b62aee700aeedade359af5a14209496a8eda
Gaprim-Choi <79235694+Gaprim-Choi@users.noreply.github.com>
2021-12-09 16:34:16 +0900

    main

--- donggune/GDsum2.py
+++ donggune/GDsum2.py
@@ -1,17 +1,12 @@
 import matplotlib
-from matplotlib import lines
 import matplotlib.pyplot as plt
 from numpy.core.numeric import NaN
 import pandas as pd
-import numpy as np
-import dataframe_image as dfi
 from PIL import Image
  
-from math import nan, pi
+from math import pi
 from matplotlib.path import Path
 from matplotlib.spines import Spine 
-from matplotlib.transforms import Affine2D
-from pandas.core.frame import DataFrame
 from pandas.core.reshape.melt import wide_to_long
 
 
@@ -63,26 +58,23 @@ matplotlib.rcParams['axes.unicode_minus'] = False
 
 k=0
 """ --------------------전체 질문자에 대한 그래프 생성---------------------- """
-for i, j in total.iterrows():
+for i, j in total.iterrows():#임의의 변수 i,j를 total List 수량만큼 반복하는 for문생성
 
     
-    labels = total.columns[:]
-    num_labels = len(labels)
+    labels = total.columns[:]#total의 컬럼과 동일한 수량,동일한 내용의 labels 리스트 생성
+    num_labels = len(labels)#labels 리스트의 수량 카운트
     
     angles = [x/float(num_labels)*(2*pi) for x in range(num_labels)] ## 각 등분점
     angles += angles[:1] ## 시작점으로 다시 돌아와야하므로 시작점 추가
-    print(angles)
-    
-    my_palette = plt.cm.get_cmap("Set2", len(total.index))
- 
-    fig = plt.figure(figsize=(8,8))
+
+    ##이미지 면적, 배경 및 도형 색상 설정
+    fig = plt.figure(figsize=(8,8)) 
     fig.set_facecolor('white')
     ax = fig.add_subplot(polar=True)
     color = 'green'
-    print(i)
-    data = total.iloc[i-1].tolist()
-    data += data[:1]
-    print (data)
+
+    data = total.iloc[i-1].tolist()# 
+    data += data[:1]# 다각형 그래프의 원점으로 돌아오는 선을 그리기 위해 시작점을 한번 더 생성
     
     if data != NaN:
         ax.set_theta_offset(pi / 2) ## 시작점
@@ -98,40 +90,40 @@ for i, j in total.iterrows():
 
         ax.plot(angles, data, color=color, linewidth=2, linestyle='solid', label='검사자 '+str(i)+' 검사 기록') ## 레이더 차트 출력
         ax.fill(angles, data, color=color, alpha=0.4) ## 도형 안쪽에 색을 채워준다.
-        for x, y, name in zip(angles, data, data):
+        for x, y, name in zip(angles, data, data): ## 도형 각 점에 data 값을 가지는 라벨 표현
             plt.text(x, y, name, bbox=box1) 
-    plt.savefig('./donggune/imgGraph/검사자'+str(i)+'의 검사그래프', dpi=300)
+    plt.savefig('./donggune/imgGraph/검사자'+str(i)+'의 검사그래프', dpi=300) ## 결과 그래프를 이미지로 저장
 k=0
 for i in total.iterrows():
     
     k= k+1
 
-    total_new = total.transpose()
-    test = total_new.sort_values(by=k, ascending=False).head(4)
-    ori_type = df[k][1]
+    total_new = total.transpose() ## 데이터 정리를 위한 배열 재 정리
+    test = total_new.sort_values(by=k, ascending=False).head(4) ## 검사자의 검사결과를 오름차순 점수로 정렬/상위 N개 추출
+    ori_type = df[k][1]## 검사자의 검사결과를 오름차순 점수로 정렬/상위 N개 추출
     사용자정보 = str(k)+'번 참가자님의 직업 코드는' + ori_type +' 입니다.' 
     검사결과 = ''
-    일치율 = []
-    타입성향 = []
-    추천직업 = []
 
     # 각 항목별 상위 분류에 대해 Text 출력을 위해 글로 정리
-    if test[k][1] == test[k][2] == test[k][3]:
-        print('재검사가 필요한 검사자 입니다.')
+    if (test[k][0] == test[k][1] == test[k][2])or(test[k][1] == test[k][2] == test[k][3]):
+        type_sum1 = '재검사가 필요한 검사자 입니다.'
     elif test[k][0] == test[k][1]:
         type_sum1 = test.index[0] + test.index[1]
         type_sum2 = test.index[1] + test.index[0]
     elif test[k][1] == test[k][2]:
         type_sum1 = test.index[0] + test.index[1]
-        type_sum2 = test.index[1] + test.index[2]
+        type_sum2 = test.index[0] + test.index[2]
     else:
         type_sum1 = test.index[0] + test.index[1]
         type_sum2 = 'nan'
-        
-    if type_sum2 in 'nan':
-        검사결과 = '당신의 적성코드는 "'+ type_sum1 +'" 입니다.'
+    
+    if type_sum1 == '재검사가 필요한 검사자 입니다.':
+        검사결과 = '재검사가 필요한 검사자 입니다.'
     else:
-        검사결과 = '당신의 적성코드는 "'+ type_sum1 + "," + type_sum2 +'" 입니다.'
+        if type_sum2 in 'nan':
+            검사결과 = '당신의 적성코드는 "'+ type_sum1 +'" 입니다.'
+        else:
+            검사결과 = '당신의 적성코드는 "'+ type_sum1 + "," + type_sum2 +'" 입니다.'
 
 
     listI = [type_sum1 , type_sum2]
@@ -153,24 +145,57 @@ for i in total.iterrows():
             str1 = '매우 낮음'
         else:
             str1 = '일치하지 않음'
-        일치율.append('당신의 직업코드 "' + ori_type + '"과(와)' + listI[i] + "과의 일치도는 "+ str1 +"입니다.")
+        strX = '당신의 직업코드 "' + ori_type + '"과(와)' + listI[i] + " 의 일치도는 "+ str1 +"입니다."
+        
+        if i == 0:
+            일치율1 = strX
+            str1 = listI[i]
+            일치율2 = 'nan'
+        else :
+            일치율2 = strX
+            str2 = listI[i]
     
-    타입성향.append(pd.DataFrame(ja.filter(ja.filter(regex=test.index[0]))))
-    타입성향.append(pd.DataFrame(ja.filter(ja.filter(regex=test.index[2]))))
-    
-    추천직업.append(pd.DataFrame(jd[(jd['코드'] == type_sum1)]))
-    if type_sum2 in 'nan':
-        추천직업 = 추천직업
-    else:
-        추천직업.append(pd.DataFrame(jd[(jd['코드'] == type_sum2)]))
+    testStr = ja.filter(ja.filter(regex=test.index[0]))
+    testStr = str(testStr.loc[0])
+    testStr = testStr.replace('\nName: 0, dtype: object','')
+    타입성향1 = testStr
+    testStr = ja.filter(ja.filter(regex=test.index[1]))
+    testStr = str(testStr.loc[0])
+    testStr = testStr.replace('\nName: 0, dtype: object','')
+    타입성향2 = testStr
+    testStr = ja.filter(ja.filter(regex=test.index[2]))
+    testStr = str(testStr.loc[0])
+    testStr = testStr.replace('\nName: 0, dtype: object','')
+    타입성향3 = testStr
 
     fig, ax =plt.subplots(1,1, figsize=(15,10))
 
-    data=[['사용자정보',사용자정보],
-          ['검사결과',검사결과],
-          ['일치도',일치율],
-          ['귀하의 성향',타입성향],
-          ['추천직업',추천직업]]
+    if 일치율2 == 'nan':
+        data=[['사용자정보',사용자정보],
+              ['검사결과',검사결과],
+              ['일치도',일치율1],
+              ['귀하의 성향1',타입성향1],
+              ['귀하의 성향2',타입성향2]]
+        m = 3
+    else:
+        m = 4
+        if str1[1] == str1[2]:
+            data=[['사용자정보',사용자정보],
+                  ['검사결과',검사결과],
+                  ['일치도',일치율1],
+                  ['일치도',일치율2],
+                  ['귀하의 성향1',타입성향1],
+                  ['귀하의 성향2',타입성향2]]
+            m = 4
+        else:
+            data=[['사용자정보',사용자정보],
+                  ['검사결과',검사결과],
+                  ['일치도',일치율1],
+                  ['일치도',일치율2],
+                  ['귀하의 성향1',타입성향1],
+                  ['귀하의 성향2',타입성향2],
+                  ['귀하의 성향2',타입성향3]]
+                  
     column_labels=["기준", "결과"]
     ax.axis('off')
     test = ax.table(cellLoc="left", cellText=data,colLabels=column_labels,loc="center")
@@ -181,10 +206,10 @@ for i in total.iterrows():
         cellDict[(0,i)].set_height(.05)
         l=0
         for j in range(1,len(data)+1):
-            if l < 4:
+            if l < m:
                 cellDict[(j,i)].set_height(.05)
             else :
-                cellDict[(j,i)].set_height(1)
+                cellDict[(j,i)].set_height(0.1)
             l=l+1
     fig = plt.gcf()
     test.auto_set_font_size(False)
@@ -209,5 +234,5 @@ for i in total.iterrows():
 
     plt.savefig('./donggune/resultImg/검사자'+str(k)+'의 검사서.png',
             bbox_inches= None,
-            dpi=100
+            dpi=300
             )
